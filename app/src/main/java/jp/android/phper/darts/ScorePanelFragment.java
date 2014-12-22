b2266bcee731e755e35f8553af65e28dc2aa9a76
mintsu <mintsu7531@gmail.com>
2014-12-22 18:37:57 +0900

    ScorePanelFragmentを追加

---
+++ app/src/main/java/jp/android/phper/darts/ScorePanelFragment.java
@@ -0,0 +1,55 @@
+package jp.android.phper.darts;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+//import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link ScorePanelFragment.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link ScorePanelFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class ScorePanelFragment extends Fragment {
+    final static int MAX_PLAYER_NUM = 4;
+    Integer playerNum;
+    TextView[] playerScore;
+    ViewGroup container;
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        //return inflater.inflate(R.layout.fragment_score_panel, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_score_panel, container, false);
+
+        playerScore = new TextView[MAX_PLAYER_NUM];
+        playerScore[0] = (TextView)view.findViewById(R.id.first_player_score);
+        playerScore[1] = (TextView)view.findViewById(R.id.second_player_score);
+        playerScore[2] = (TextView)view.findViewById(R.id.third_player_score);
+        playerScore[3] = (TextView)view.findViewById(R.id.fourth_player_score);
+
+        this.container = container;
+
+        return view;
+    }
+
+    public void setPlayerNum(int num) {
+        playerNum = num;
+    }
+
+    public void setPlayerScoreDisplay(int index, int flag) {
+        playerScore[index].setVisibility(flag);
+    }
+
+
+}
